a70b82d1c3946e1cb10a8116855b89d685ea6322
MR.IBAY <ibayjimwell@gmail.com>
2025-04-27 20:25:14 +0800

    I start building the core functionality of the web app
    Products Listing
    Add to Cart
    Cart Listing
    Searching

---
+++ database.js
@@ -0,0 +1,143 @@
+const pg = require('pg');
+const { Pool } = pg;
+require('dotenv').config(); // Load environment variables
+const bcrypt = require('bcrypt'); // For password hashing
+
+// Database class It's a responsible to manipulate the database connection and queries
+class Database {
+    constructor() {
+        this.pool = new Pool({
+            user: process.env.USER,
+            host: process.env.HOST,
+            database: process.env.DATABASE,
+            password: process.env.PASSWORD,
+            port: process.env.PORT,
+        });
+    }
+
+    // Method to get the fashions ramdomly
+    async getFashions(limit = null) {
+        try {
+            
+            if (limit) {
+                const result = await this.pool.query('SELECT * FROM style_diaries_fashions ORDER BY RANDOM() LIMIT $1', [limit]);
+                return result.rows;
+            } else {
+                const result = await this.pool.query('SELECT * FROM style_diaries_fashions ORDER BY RANDOM()');
+                return result.rows;
+            }
+
+        } catch (error) {
+            console.error('Error in getFashions:', error);
+        }
+    }
+
+    // Method to Signup a user
+    async signupUser(username, password, fullname) {
+        try {
+            const saltRound = 10;
+            const hashedPassword = await bcrypt.hash(password, saltRound); // Hash the password
+            const result = await this.pool.query('INSERT INTO style_diaries_users (username, password, fullname) VALUES ($1, $2, $3)', [username, hashedPassword, fullname]);
+
+            if (result.rowCount > 0) {
+                return true; // User successfully signed up
+            } else {
+                return false; // User signup failed
+            }
+
+        } catch (error) {
+            console.error('Error in signupUser:', error);
+        }
+    }
+
+    // Method to login a user
+    async loginUser(username, password) {
+        try {
+            const result = await this.pool.query('SELECT * FROM style_diaries_users WHERE username = $1', [username]);
+            if (result.rows.length > 0) {
+                const user = result.rows[0];
+                const isMatch = await bcrypt.compare(password, user.password); // Compare the hashed password
+                if (isMatch) {
+                    return user; // Return the user object if login is successful
+                } else {
+                    return false; // Password does not match
+                }
+            } else {
+                return false; // User not found
+            }
+        } catch (error) {
+            console.error('Error in loginUser:', error);
+            return false;
+        }
+    }
+
+    // Method to get a products
+    async getProducts(category) {
+        try {
+            const result = await this.pool.query('SELECT * FROM style_diaries_products WHERE category = $1', [category]);
+            return result.rows; // Return the products for the specified category
+        } catch (error) {
+            console.error('Error in getProducts:', error);
+        }
+    }
+
+    // Method to add to cart
+    async addToCart(userId, productId) {
+        try {
+            const result = await this.pool.query('INSERT INTO style_diaries_cart (user_id, product_id) VALUES ($1, $2)', [userId, productId]);
+            return result.rowCount > 0; // Return true if the product was added to the cart successfully
+        } catch (error) {
+            console.error('Error in addToCart:', error);
+            return false; // Return false if there was an error
+        }
+    }
+
+    // Method to get cart items
+    async getCartItems(userId) {
+        try {
+            const result = await this.pool.query('SELECT c.id AS cart_id, p.* FROM style_diaries_cart c JOIN style_diaries_products p ON c.product_id = p.id  WHERE c.user_id = $1', [userId]);
+            return result.rows; // Return the cart items for the specified user
+        } catch (error) {
+            console.error('Error in getCartItems:', error);
+        }
+    }
+
+    // Method to remove from cart
+    async removeFromCart(cartId) {
+        try {
+            const result = await this.pool.query('DELETE FROM style_diaries_cart WHERE id = $1', [cartId]);
+            return result.rowCount > 0; // Return true if the product was removed from the cart successfully
+        } catch (error) {
+            console.error('Error in removeFromCart:', error);
+            return false; // Return false if there was an error
+        }
+    }
+
+    // Method to remove all items from cart for a user
+    async removeAllFromCart(userId) {
+        try {
+            const result = await this.pool.query('DELETE FROM style_diaries_cart WHERE user_id = $1', [userId]);
+            return result.rowCount > 0; // Return true if all items were removed from the cart successfully
+        } catch (error) {
+            console.error('Error in removeAllFromCart:', error);
+            return false; // Return false if there was an error
+        }
+    }
+
+    // Search for products 
+    async searchProducts(query) {
+        try {
+            const result = await this.pool.query('SELECT * FROM style_diaries_products WHERE name ILIKE $1 OR category ILIKE $1', [`%${query}%`]);
+            return result.rows; // Return the products that match the search query
+        } catch (error) {
+            console.error('Error in searchProducts:', error);
+        }
+    }
+
+}
+
+module.exports = Database; // Export the Database class for use in other files
+
+
+            
+           
--- index.js
+++ index.js
@@ -5,44 +5,88 @@ const path = require('path');
 app.set('view engine', 'ejs'); // Set the view engine to ejs it's help to render the html file and make it reusable
 app.set('views', path.join(__dirname, 'views')); // Set the views directory
 app.use(express.static(path.join(__dirname, 'static'))) // Serve static files from the public directory
+let database = require('./database.js'); // Import the database class
+const { Query } = require('pg');
+let db = new database(); // Create an instance of the database class or connect to the database
 
 // Global variables
-const User = {
-    username: 'JohnDoe', // Example user data
-    password: 'password123' // Example password data
-};
+let User = null;
+let Search = null;
 
 // Middlewares
 app.use((req, res, next) => {
     res.locals.currentPath = req.path; // make available in all EJS files
     next();
 });
+// For JSON requests
+app.use(express.json());
+// For form data (from HTML forms)
+app.use(express.urlencoded({ extended: true }));
 
 // Initial route
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     if (!User) {
         res.redirect('/login'); // Redirect to login page if user is not logged in
     } else {
-        res.render('index');
+        let fashions = await db.getFashions(12);
+        res.render('index', { fashions: fashions });
     }
 });
 
 // Login route
 app.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', { error: null });
+});
+app.post('/login', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    let login = await db.loginUser(username, password); // Call the login method from the database class
+
+    if (login) {
+        User = login; // Set the global User variable to the logged-in user
+        res.redirect('/'); // Redirect to the home page after successful login
+    } else {
+        res.render('login', { error: 'Invalid username or password.' });
+    }
+
 });
 
 // Signup route
 app.get('/signup', (req, res) => {
-    res.render('signup');
+    res.render('signup', { error: null });
+});
+app.post('/signup', async (req, res) => {
+    const fullname = req.body.fullname;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Basic validation for username and password
+    if (username.length >= 3 && password.length >= 8 && fullname.length >= 3) {
+        let signup = await db.signupUser(username, password, fullname); // Call the signup method from the database class
+
+        if (signup) {
+            let login = await db.loginUser(username, password); // Call the login method from the database class
+            if (login) {
+                User = login; // Set the global User variable to the logged-in user
+                res.redirect('/'); 
+            }
+        } else {
+            res.render('signup', { error: 'Signup failed. Please try again.' });
+        }
+        
+    } else {
+        // Render the signup page with an error message
+        res.render('signup', { error: 'Fullname and Username must be at least 3 characters and password at least 8 characters.' });
+    }
 });
 
 // Fashions route
-app.get('/fashions', (req, res) => {
+app.get('/fashions', async (req, res) => {
     if (!User) {
         res.redirect('/login'); // Redirect to login page if user is not logged in
     } else {
-        res.render('fashions');
+        let fashions = await db.getFashions();
+        res.render('fashions', { fashions: fashions });
     }
 });
 
@@ -51,25 +95,121 @@ app.get('/user/profile', (req, res) => {
     if (!User) {
         res.redirect('/login'); // Redirect to login page if user is not logged in
     } else {
-        res.render('profile');
+        res.render('profile', { user: User });
     }
 });
 
 // Cart route
-app.get('/shop/Cart', (req, res) => {
+app.get('/shop/Cart', async (req, res) => {
     if (!User) {
         res.redirect('/login'); // Redirect to login page if user is not logged in
     } else {
-        res.render('cart');
+        let cart = await db.getCartItems(User.id); // Get the user's cart from the database
+        let subtotal = 0;
+        let shipping = 10;
+        let total = 0;
+
+        if (cart) {
+            for (let i = 0; i < cart.length; i++) {
+                subtotal += parseFloat(cart[i].price); // Calculate the subtotal
+            }
+            total = subtotal + shipping; // Calculate the total including shipping
+        }
+
+        res.render('cart', {cart: cart, subtotal: subtotal, shipping: shipping, total: total}); // Render the cart page with the user's cart items
     }
 });
 
 // Shop route
-app.get('/shop/:category', (req, res) => {
+app.get('/shop/:category', async (req, res) => {
     if (!User) {
         res.redirect('/login'); // Redirect to login page if user is not logged in
     } else {
-        res.render('shop', { category: req.params.category });
+
+        let category = req.params.category;
+        let products = await db.getProducts(category); // Get products for the specified category
+
+        res.render('shop', { user: User, category: category, products: products });
+    }
+});
+
+// Add to cart route
+app.post('/add/to/cart/:user_id/:category/:product_id', async (req, res) => {
+    if (!User) {
+        res.redirect('/login'); // Redirect to login page if user is not logged in
+    } else {
+        let user_id = req.params.user_id;
+        let category = req.params.category;
+        let product_id = req.params.product_id;
+
+        let addToCart = await db.addToCart(user_id, product_id); // Call the add to cart method from the database class
+
+        if (addToCart) {
+            if (category == 'search') {
+               res.status(303).redirect(307, `/search/${Search}`); // Redirect to the search results page using POST method
+            } else {
+                res.redirect(`/shop/${category}/#${product_id}`);
+            }
+        } else {
+            res.status(500).send('Error adding to cart.'); // Send an error response if adding to cart fails
+        }
+    }
+});
+
+// Remove from cart route
+app.post('/remove/from/cart/:cart_id', async (req, res) => {
+    if (!User) {
+        res.redirect('/login'); // Redirect to login page if user is not logged in
+    } else {
+        let cart_id = req.params.cart_id;
+        let removeFromCart = await db.removeFromCart(cart_id); // Call the remove from cart method from the database class
+
+        if (removeFromCart) {
+            res.redirect('/shop/Cart'); // Redirect to the cart page after removing the item
+        } else {
+            res.status(500).send('Error removing from cart.'); // Send an error response if removing from cart fails
+        }
+    }
+});
+
+// Checkout route
+app.post('/checkout', async (req, res) => {
+    if (!User) {
+        res.redirect('/login'); // Redirect to login page if user is not logged in
+    } else {
+        let removeAllFromCart = await db.removeAllFromCart(User.id); // Call the remove all from cart method from the database class
+        if (removeAllFromCart) {
+            res.redirect('/'); // Redirect to the home page after checkout
+        } else {
+            res.status(500).send('Error during checkout.'); // Send an error response if checkout fails
+        }
+    }
+});
+
+// Signout route
+app.post('/signout', (req, res) => {
+    User = null; // Clear the global User variable to log out the user
+    res.redirect('/login'); // Redirect to the login page after signing out
+});
+
+// Search route
+app.post('/search/:query', async (req, res) => {
+    if (!User) {
+        res.redirect('/login'); // Redirect to login page if user is not logged in
+    } else {
+        if (req.params.query == '0') {
+           Search = req.body.search;
+        } else {
+            Search = req.params.query;
+        }
+
+        let products = await db.searchProducts(Search); // Call the search method from the database class
+
+        if (products) {
+            res.render('search', { products: products, user: User }); // Render the index page with the search results
+        } else {
+            res.status(500).send('Error searching for products.'); // Send an error response if search fails
+        }
     }
 });
 
@@ -78,4 +218,4 @@ app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
 
-module.exports = app; // Export the app to detect in deploying
+module.exports = app; // Export the app to detect in hosting
